74ef025ff0d4c9266d57bdbfd597425952ba63a8
Changsheng Liu <liucs90@hotmail.com>
2017-12-07 14:03:20 -0800

    task 1

--- task1.py
+++ task1.py
@@ -12,43 +12,46 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
+ 
 
-images = glob.glob('*.png')
+img = cv2.imread('calibration.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-for fname in images:
-	img = cv2.imread(fname)
-	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+# Find the chess board corners
+ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
-	# Find the chess board corners
-	ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
+# If found, add object points, image points (after refining them)
 
-	# If found, add object points, image points (after refining them)
-	if ret == True:
-		objpoints.append(objp)
+if ret == True:
+	objpoints.append(objp)
 
-		corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-		imgpoints.append(corners2)
+	corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+	imgpoints.append(corners2)
 
-		# Draw and display the corners
-		img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
-		cv2.imshow('img',img)
-		cv2.waitKey(500)
+	# Draw and display the corners
+	img = cv2.drawChessboardCorners(img, (400,400), corners2,ret)
+	cv2.imshow('img',img)
+	cv2.waitKey(500)
 
-cv2.destroyAllWindows()
-
-
-#-------------------
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-#-------------------
-img = cv2.imread('calibration.jpeg')
+
+
+img = cv2.imread('left.jpg')
 h,  w = img.shape[:2]
-newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-#-------------------
 # undistort
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
-
+mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv2.remap(img, mapx, mapy, cv2.INTER_LINEAR)
 # crop the image
-x,y,w,h = roi
+x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibresult.png',dst)
+cv2.imwrite('calibresult.png', dst)
+
+
+mean_error = 0
+for i in range(len(objpoints)):
+	imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+	error = cv2.norm(imgpoints[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+	mean_error += error
+print( "total error: {}".format(mean_error/len(objpoints)) )
